575936eaff653784c636003289cfb7c89d99a46a
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-06-25 10:53:42 -0500

    Initial Commit

---
+++ NYPeopleFeed/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NYPeopleFeed
+//
+//  Created by Diego de Jesus Ramirez on 25/06/20.
+//  Copyright © 2020 DiegoRamirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ NYPeopleFeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NYPeopleFeed
+//
+//  Created by Diego de Jesus Ramirez on 25/06/20.
+//  Copyright © 2020 DiegoRamirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
